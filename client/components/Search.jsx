e6368c34186021a0facf53aa92c251b0d693d703
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-05-21 13:12:53 -0700

    making api calls to moviedb to retreive data

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 
 const Search = () => {
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    const query = "Superman";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=KEYHERE&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">
         Movie Name
       </label>
