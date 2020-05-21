1428e2df43b9f0e46e073b2be44f458615b148a5
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-05-21 16:58:35 -0700

    retrieving image icons of movies searched from api calls

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -1,35 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
+  const [query, setQuery] = useState("");
+  //create state for movies and update state when appropriate
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (e) => {
     e.preventDefault();
-    const query = "Superman";
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=KEYHERE&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
       const res = await fetch(url);
       const data = await res.json();
-      console.log(data);
+      setMovies(data.results);
+      console.log(data.results);
     } catch (err) {
       console.log(err);
     }
   };
 
   return (
-    <form className="form" onSubmit={searchMovies}>
-      <label className="label" htmlFor="query">
-        Movie Name
-      </label>
-      <input
-        className="input"
-        name="query"
-        placeholder="Search For A Movie"
-      ></input>
-      <button className="button" type="submit">
-        Search
-      </button>
-    </form>
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          name="query"
+          placeholder="Search For A Movie"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        ></input>
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+        {movies.map((movie) => (
+          <div className="card">
+            <img
+              className="card--image"
+              src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+            />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
